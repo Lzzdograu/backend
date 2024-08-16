376a05df7a113e3b3eac86ffd034ecfe324b9eaf
Lzzdograu <luis.silva.senai404@gmail.com>
2024-08-16 09:23:38 -0300

    Add files via upload

---
+++ php/return.php
@@ -0,0 +1,11 @@
+<?php  
+//declaração de uma função que retorna um valor
+function soma ($a, $b) {
+    return $a + $b;
+}
+
+// utilizando o valor retornafo pela função
+$resultado = soma (5,3); // $resultado será 8
+echo $resultado; // saida:8
+
+?>
