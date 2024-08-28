c7eee074b4492c79dabe350bfb91b0ce03381c75
Lzzdograu <luis.silva.senai404@gmail.com>
2024-08-28 15:52:34 -0300

    Add files via upload

---
+++ php/array-exemplo.php
@@ -0,0 +1,9 @@
+<?php
+
+$lista_frutas = array("Banana", "maça", "Morango", "Uva");
+
+echo $lista_frutas[1];// isso mostraria "maça"
+
+
+
+?>
---
+++ php/array-exeplo3.php
@@ -0,0 +1,22 @@
+<?php
+
+//criando a aray lista de frutas 
+$listas_frutas = ["Banana";"Maça","Morango","Uva"];
+
+echo '<pre>';
+print_r ($lista_frutas);
+echo $listas_frutas[o];
+echo "<hr>";
+echo "</pre>";
+
+echo '<pre>';
+//Alterando o valor 0 -> Banana para Melãncia 
+$lista_frutas[0] = "Melãncia";
+print_r ($lista_frutas);
+echo "<br>";
+
+echo $lista_frutas[0];
+
+
+
+?>
